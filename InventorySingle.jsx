7e55b784f48de05a346b7999c6fde7ef67fd9def
cgaudry <gaudry-c4@webmail.uwinnipeg.ca>
2017-01-22 12:52:02 -0600

    Inventory Item Edit

---
+++ InventorySingle.jsx
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+import InventoryDetail from './InventoryDetail.jsx';
+
+export default class InventorySingle extends Component {
+
+	constructor() {
+		super();
+		this.state = {editMode: undefined};
+		this.setNormalMode = this.setNormalMode.bind(this);
+		this.setEditMode = this.setEditMode.bind(this);
+	}
+
+	toggleChecked() {
+		Meteor.call('toggleResolution', this.props.inventoryItem);
+	}
+
+	componentWillMount() {
+		this.setState({editMode: false});
+	}
+
+	componentWillUnmount() {
+		Blaze.remove(this.view);
+	}
+
+	setNormalMode() {
+		this.setState({editMode: false});
+	}
+
+
+	setEditMode() {
+		this.setState({editMode: true});
+	}
+
+	deleteInventoryItem() {
+		Meteor.call('deleteInventoryItem', this.props.inventoryItem);
+	}
+
+	editInventoryItem() {
+		this.setState({editMode: false});
+		Meteor.call('editInventoryItem', this.props.inventoryItem, inventoryItemName.value, inventoryItemQuantity.value);
+	}
+
+	normalView(){
+		return(
+			<div>
+			<a href={`/inventory/${this.props.inventoryItem._id}`}>
+					ItemName - {this.props.inventoryItem.inventoryItemName} Quantity -  {this.props.inventoryItem.inventoryItemQuantity}
+				</a>
+
+				{status}
+				<button className="btn-text"
+					onClick={this.setEditMode.bind(this)}>					
+					Edit
+				</button>
+				<button className="btn-cancel"
+					onClick={this.deleteInventoryItem.bind(this)}>
+					&times; 
+				</button>
+				</div>
+			);
+	}
+
+	editView(){
+		return(
+			<div>
+					ItemName -
+					<input 
+						id="itemName"
+						type="text" 
+						ref={(input) => {inventoryItemName = input;}}
+						defaultValue={this.props.inventoryItem.inventoryItemName}
+					/>  
+					Quantity -  
+					<input 
+						id="itemQuantity"
+						type="number" 
+						ref={(input) => {inventoryItemQuantity = input;}}
+						defaultValue={this.props.inventoryItem.inventoryItemQuantity}
+					/>
+
+				{status}
+				<button className="btn-text"
+					onClick={this.editInventoryItem.bind(this)}>
+					Apply
+				</button>
+				<button className="btn-cancel"
+					onClick={this.setNormalMode.bind(this)}>					
+					Cancel 
+				</button>
+			</div>
+			);
+	}
+
+	render() {
+		const resolutionClass = this.props.inventoryItem.complete ? "checked" : "";
+		const status = this.props.inventoryItem.complete ? <span className="completed">Completed</span> : '';
+
+		return (
+			<div>
+			<li className={resolutionClass}>
+			<input type="checkbox"
+				readOnly={true}
+				checked={this.props.inventoryItem.complete}
+				onClick={this.toggleChecked.bind(this)} />
+			{ this.state.editMode ? this.editView() :this.normalView() }
+			</li>
+			</div>
+		)
+	}
+}
---
+++ methods.js
@@ -0,0 +1,97 @@
+Meteor.methods({
+
+	toggleResolution(resolution) {
+		if(Meteor.userId() !== resolution.user) {
+			throw new Meteor.Error('Not authorized')
+		}
+		Resolutions.update(resolution._id, {
+			$set: {complete: !resolution.complete}
+		})
+	},
+
+
+
+	addInventoryItem(inventoryItemId, inventoryItemName, inventoryItemQuantity) {
+		if(!Meteor.userId()) {
+			throw new Meteor.Error('Not authorized')
+		}
+
+		entry = Inventory.findOne({inventoryItemId: inventoryItemId})
+		//update quantity if item exists, add new listing if not
+		if(entry) {
+			newQuantity = parseInt(entry.inventoryItemQuantity) + parseInt(inventoryItemQuantity)
+			Inventory.update(
+				{_id: entry._id},
+				{$set: {inventoryItemQuantity: newQuantity}}
+				)
+		} else {
+			Inventory.insert({
+				inventoryItemId: inventoryItemId,
+				inventoryItemName: inventoryItemName,
+				inventoryItemQuantity: parseInt(inventoryItemQuantity),
+				complete: false,
+				createdAt: new Date(),
+				user: Meteor.userId()
+			})
+		}
+	},
+
+	editInventoryItem(inventoryItem, inventoryItemName, inventoryItemQuantity) {
+		if(!Meteor.userId()) {
+			throw new Meteor.Error('Not authorized')
+		}
+		entry = Inventory.findOne({_id: inventoryItem._id})
+		if(entry) {
+			console.log("Attempting database update...");
+			newQuantity = parseInt(inventoryItemQuantity)
+			Inventory.update(
+				{_id: entry._id},
+				{$set: {inventoryItemName: inventoryItemName}}
+				)
+			Inventory.update(
+				{_id: entry._id},
+				{$set: {inventoryItemQuantity: newQuantity}}
+				)
+		} else {
+			throw new Meteor.Error('Invalid ID')
+		}
+	},
+
+	deleteInventoryItem(inventoryItem) {
+		//can only delete items user inserted
+		//might have to change
+		if(Meteor.userId() !== inventoryItem.user) {
+			throw new Meteor.Error('Not authorized')
+		}
+		Inventory.remove(inventoryItem._id)
+	},
+
+	addJob(invoice, firstName, lastName, address, phoneNumber, email, jobTypeCode,
+			estimateCost, estimateParts, estimateEmployee, installCost, installParts, installEmployee, vehicleId, mileage) {
+		if(!Meteor.userId()) {
+			throw new Meteor.Error('Not authorized')
+		}
+		
+			Jobs.insert({
+				invoice: invoice,
+				firstName: firstName,
+				lastName: parseInt(lastName),
+				address: address,
+				phoneNumber: parseInt(phoneNumber),
+				email: email,
+				jobTypeCode: jobTypeCode,
+				estimateCost: parseInt(estimateCost),
+				estimateParts: estimateParts,
+				estimateEmployee: estimateEmployee,
+				installCost: parseInt(installCost),
+				installParts: installParts,
+				installEmployee: installEmployee,
+				vehicleId: vehicleId,
+				mileage: parseInt(mileage),
+				complete: false,
+				createdAt: new Date(),
+				user: Meteor.userId()
+			})
+		
+	}
+});
