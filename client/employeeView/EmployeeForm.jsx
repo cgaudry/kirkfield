e0bceb96a5bd8f72843044919a351e98d4d1099d
trevorha <harrisatrevor@gmail.com>
2017-02-27 21:24:42 -0600

    Added a few more fields on form

--- client/employeeView/EmployeeForm.jsx
+++ client/employeeView/EmployeeForm.jsx
@@ -35,19 +35,70 @@ export default class EmployeeForm extends Component {
 					/>
 					</div>
 					</div>
+					
 					<div className="form-group">
-					<label className="control-label col-sm-2" htmlFor="employeeName">Employee Name:</label>
-					<div className="col-sm-10">
+					<label className="control-label col-sm-2" htmlFor="employeeFirstName">First Name:</label>
+					<div className="col-sm-4">
 					<input 
 						className="form-control"
-						id="employeeName"
+						id="employeeFirstName"
 						type="text" 
-						ref="employeeName"
-						placeholder="Employee Name"
+						ref="employeeFirstName"
+						placeholder="First Name"
+					/>
+					</div>
+					<label className="control-label col-sm-2" htmlFor="employeeLastName">Last Name:</label>
+					<div className="col-sm-4">
+					<input 
+						className="form-control"
+						id="employeeLastName"
+						type="text" 
+						ref="employeeLastName"
+						placeholder="Last Name"
+					/>
+					</div>
+					</div>
+					
+					<div className="form-group">
+					<label className="control-label col-sm-2" htmlFor="employeeStartDate">Start Date:</label>
+					<div className="col-sm-2">
+					<input
+						className="form-control"
+						id="employeeStartDate"
+						type="date"
+						ref="employeeStartDate"
+						placeholder="Start Date"
+					/>
+					</div>
+					<label className="control-label col-sm-2" htmlFor="employeeExperience">Experience (Years):</label>
+					<div className="col-sm-2">
+					<input
+						className="form-control"
+						id="employeeExperience"
+						type="number"
+						ref="employeeExperience"
+						placeholder="Experience"
+						min="0"
+						max="100"
+						step="1"
+					/>
+					</div>
+					<label className="control-label col-sm-2" htmlFor="employeeHourlyRate">Hourly Rate ($):</label>
+					<div className="col-sm-2">
+					<input
+						className="form-control"
+						id="employeeHourlyRate"
+						type="number"
+						ref="employeeHourlyRate"
+						placeholder="Hourly Rate"
+						min="10.00"
+						max="1000.00"
+						step="0.01"
 					/>
 					</div>
 					
 					</div>
+					
 					<input type="submit" className="btn btn-primary pull-right"/>
 				</form>
 			)
